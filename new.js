6c2ae43cca394008d911b377bce659329120fb06
Eldohub Academy <academy@eldohub.co.ke>
2024-08-28 15:41:09 +0300

    tasks

--- new.js
+++ new.js
@@ -1,3 +1,10 @@
-console.log("Hello there!!");
+const password = "fdfjdfkdhfd"
 
-alert("We are watching!!!!!");
+if(password.length < 6 ){
+    console.log("Short password");
+    
+}
+else{
+    console.log("login ");
+    
+}
